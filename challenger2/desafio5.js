8d88b329e93d6078ab04d1901a3e5225192762cf
anderson espindola <anderson.espindola@senior.com.br>
2020-11-05 22:37:42 -0300

    completed quetion 5 of challenge 2

---
+++ challenger2/desafio5.js
@@ -0,0 +1,10 @@
+const dataBase = require('../db/index')
+
+const users = dataBase.map(user => ({
+    name: user.name,
+    repos: user.public_repos
+})).filter(userFiltered => userFiltered.repos > 10).sort((a, b) => {
+    return (a.repos < b.repos) - 1
+})
+
+console.log(users)
