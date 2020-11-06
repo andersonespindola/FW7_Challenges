5de41600d6f1672d6c7b4b25368986c5a3b7468f
anderson espindola <anderson.espindola@senior.com.br>
2020-11-05 22:02:10 -0300

    adjust in question 3, question 4 ok

--- challenger2/desafio3.js
+++ challenger2/desafio3.js
@@ -3,7 +3,7 @@ const dataBase = require('../db/index')
 const sortArray = dataBase.map(array => ({
     nome: array.name,
     data: array.created_at
-})).sort(function(a, b) {
+})).sort((a, b) => {
     return (a.data < b.data) - 1
 })
 
---
+++ challenger2/desafio4.js
@@ -0,0 +1,7 @@
+const dataBase = require('../db/index')
+
+const biggestName = dataBase.reduce((accumulator, currentValue) =>
+    accumulator.name.length > currentValue.name.length ? accumulator : currentValue
+)
+
+console.log(biggestName.name)
